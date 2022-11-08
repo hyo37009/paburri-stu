435d30104fba82463da3ca0f2dbc341c760dec55
지예 데스크탑 <hyo37009@gmail.com>
2022-11-09 00:23:48 +0900

    .

---
+++ subway.py
@@ -0,0 +1,27 @@
+class station:
+    def __init__(self, name:str, line:list):
+        # 여기를 작성해주세요. 변수 이름: name, line, hwan, neighbor
+
+    def __str__(self):
+        return self.name
+
+    def connection(self, station:str, prev=0):
+        pass
+
+    def prevStation(self):
+        pass
+
+    def nextStation(self):
+        pass
+
+
+    def printconnection(self):
+        pass
+
+
+    def printline(self):
+        pass
+
+
+class line:
+    pass
